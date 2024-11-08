6a01b15207726b2e6d97b08bba75aba554163de3
Kimhyunmook <gusanr4200@naver.com>
2024-11-08 14:57:00 +0900

    .

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,7 +6,9 @@ export default function Home() {
     <Layout>
       <WhiteContainer title={"최근 조회한 스터디"} titleMargin={"40px"}>
         <Cards>
-          <Card></Card>
+          <Card point={202} in userName="이유디" name="의 UX 스터디">Slow And Steady Wins The Race!!</Card>
+          <Card point={202} in userName="이유디" name="의 UX 스터디">Slow And Steady Wins The Race!!</Card>
+          <Card point={202} in userName="이유디" name="의 UX 스터디">Slow And Steady Wins The Race!!</Card>
         </Cards>
       </WhiteContainer>
     </Layout>
--- src/shared/component/Card.jsx
+++ src/shared/component/Card.jsx
@@ -4,28 +4,36 @@ export function Card({
   point = 0,
   emogi = [{ type: "", value: 0 }],
   name = "",
+  userName = "",
   inProgress = 0,
   children,
 }) {
   return (
     <div className="card" style={{ background: bg }}>
       <div className="top">
-        <h3 className="name">{name}</h3>
+        <h3 className="name">
+          <span>{userName}</span>
+          {name}
+        </h3>
         <div className="point">
           <img src="/img/point.png" alt="포인트" />
-          {point}P 획득
+          <p>
+            <span>{point}</span>P 획득
+          </p>
         </div>
       </div>
       <p className="inProgress">{inProgress}일째 진행 중</p>
       <p className="content">{children}</p>
-      {emogi.map((v, index) => {
-        return (
-          <div className="emogi" key={index}>
-            <img src="/img/point.png" alt={v.type} />
-            <p>{v.value}</p>
-          </div>
-        );
-      })}
+      <div className="emogis">
+        {emogi.map((v, index) => {
+          return (
+            <div className="emogi" key={index}>
+              <img src="/img/point.png" alt={v.type} />
+              <p>{v.value}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
