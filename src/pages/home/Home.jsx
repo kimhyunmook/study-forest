a7b22f8db607120c8b6fa5da8b5a7a4790d3810f
Kimhyunmook <gusanr4200@naver.com>
2024-11-08 13:36:07 +0900

    card 작업

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,3 +1,14 @@
-export default function Example() {
-  return <div>dmdmdd</div>;
+import Cards, { Card } from "../../shared/component/Card";
+import Layout, { WhiteContainer } from "../../shared/component/Layout";
+
+export default function Home() {
+  return (
+    <Layout>
+      <WhiteContainer title={"최근 조회한 스터디"} titleMargin={"40px"}>
+        <Cards>
+          <Card></Card>
+        </Cards>
+      </WhiteContainer>
+    </Layout>
+  );
 }
