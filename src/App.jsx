1fe07a7210a147e37a3d6851300be26bda056375
JeonJun202 <jjg010202@gmail.com>
2024-11-12 17:07:43 +0900

    오늘의 집중

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Header from "./shared/component/header";
 import DetailPage from "./detailPage/DetailPage";
+import FocusPage from "./focusPage/FocusPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/DetailPage" element={<DetailPage />} />
+          <Route path="/FocusPage" element={<FocusPage />} />
         </Routes>
       </div>
     </BrowserRouter>
