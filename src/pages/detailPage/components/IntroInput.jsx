d056db3dfb6f7b48b6fe1e406b9cfef0428820bb
soohwanpak <qoiuytnghks4@gmail.com>
2024-11-18 10:28:50 +0900

    박수환:상세페이지

--- src/pages/detailPage/components/IntroInput.jsx
+++ src/pages/detailPage/components/IntroInput.jsx
@@ -1,16 +1,16 @@
-export function IntroInput({ intro, setIntro }) {
+export function IntroInput({ introduce, setIntroduce }) {
     return (
         <div className="detail-page-op">
             <div className="detail-page-introduce">소개</div>
             <textarea
-                value={intro}
-                onChange={(e) => setIntro(e.target.value)}
+                value={introduce}
+                onChange={(e) => setIntroduce(e.target.value)}
                 className="detail-page-introduce-input"
                 placeholder="소개 멘트를 작성해주세요"
             />
             <div
                 className="input-err"
-                style={{ display: intro & (intro.length < 0) ? "block" : "none" }}
+                style={{ display: (introduce?.length || 0) < 0 ? "block" : "none" }}
             >
                 소개 멘트를 작성해주세요
             </div>
