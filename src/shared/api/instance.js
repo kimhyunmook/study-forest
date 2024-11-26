00e36ff5fd7f7b68caaf8ca977354011ca91367b
soohwanpak <qoiuytnghks4@gmail.com>
2024-11-27 07:17:39 +0900

    instance 수정

--- src/shared/api/instance.js
+++ src/shared/api/instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const baseURL = "https://study-forest-be.onrender.com";
-const baseURL = "http://localhost:8000";
+const baseURL = "https://study-forest-be.onrender.com";
+// const baseURL = "http://localhost:8000";
 const instance = axios.create({
   baseURL,
   headers: {
